619aca7674aecb6c849aa19ff5b472614effa2f8
Littlerou <izzymelvin4@gmail.com>
2022-08-24 16:33:49 +0100

    add margin, hide search bar, google image smaller, search bar larger, links have text-decoration and add image eventlistener

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-
+// Retrieve element
 const random = document.querySelector("#lucky")
 const search = document.querySelector('#search')
 const div1 = document.querySelector("#link1")
@@ -13,13 +13,35 @@ const div9 = document.querySelector("#link9")
 const div10 = document.querySelector("#link10")
 const searchbar = document.querySelector("#searchbar")
 const stylesheet = document.querySelector("#stylesheet")
+const image = document.querySelector("#googleLogo")
 
+// Event Listeners
+// Event listener for the 'I'm feeling lucky button' - should generate a new tab with a random link
 random.addEventListener("click", () => {
     fetch("http://localhost:3000/results/random")
     .then( res => res.json())
     .then(openLink)
 })
 
+//Event listener for the 'search' button - should generate the search results
+search.addEventListener("click", () => {
+    fetch("http://localhost:3000/results")
+    .then(res => res.json())
+    .then(generateLinks)
+    searchbar.setAttribute("value", "reflection nebula")
+})
+
+// Event listener for search bar - should input reflection nebula when key pressed
+searchbar.addEventListener("keypress", (event) => {
+    event.preventDefault()
+    searchbar.setAttribute("value", "reflection nebula")
+    i = 1
+    console.log("i am working")
+})
+
+image.addEventListener("click",(removeLinks));
+
+// Functions
 function openLink (data) {
     open(data.url)
 }
@@ -30,32 +52,37 @@ function generateLinks (data) {
         const url = document.createElement("p")
         const title = document.createElement("a")
         const desc = document.createElement("p")
-    title.setAttribute("class", "title")
-    title.setAttribute("href", data[i].url)
-    url.setAttribute("class", "url")
-    desc.setAttribute("class", "description")
-    title.textContent = data[i].title
-    url.textContent= data[i].url
-    desc.textContent=data[i].description
-    divs[i].append(url)
-    divs[i].append(title)
-    divs[i].append(desc)
+        title.setAttribute("class", "title")
+        title.setAttribute("href", data[i].url)
+        url.setAttribute("class", "url")
+        desc.setAttribute("class", "description")
+        title.textContent = data[i].title
+        url.textContent= data[i].url
+        desc.textContent=data[i].description
+        divs[i].append(url)
+        divs[i].append(title)
+        divs[i].append(desc)
     }
     stylesheet.setAttribute("href", "styleSheet.css")
     random.setAttribute("hidden", "hidden");
+    search.setAttribute("hidden", "hidden");
 }
 
 
-search.addEventListener("click", () => {
-    fetch("http://localhost:3000/results")
-    .then(res => res.json())
-    .then(generateLinks)
-    searchbar.setAttribute("value", "reflection nebula")
-})
+function removeLinks () {
+    div1.remove();
+    div2.remove();
+    div3.remove();
+    div4.remove();
+    div5.remove();
+    div6.remove();
+    div7.remove();
+    div8.remove();
+    div9.remove();
+    div10.remove();
+    stylesheet.setAttribute("href", "style.css")
+    random.removeAttribute("hidden", "hidden");
+    search.removeAttribute("hidden", "hidden");
+    searchbar.removeAttribute("value", "reflection nebula")
+}
 
-searchbar.addEventListener("keypress", (event) => {
-    event.preventDefault()
-    searchbar.setAttribute("value", "reflection nebula")
-    i = 1
-    console.log("i am working")
-})
